004d18677363840daddf041fc27dbbc146da6e05
SpiriDs <spirids@live.de>
2020-09-23 14:25:03 +0700

    Add the app to shopify devshop

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "38c1a317b6d84abefc4d";
+/******/ 	var hotCurrentHash = "82c3c04a70c962e2f463";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
